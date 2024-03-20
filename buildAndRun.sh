c68595d975d8bb4091d2f8c64d774c7d1178b678
Ravil.Abdulkhairov <Ravil.Abdulkhairov@x5.ru>
2024-03-20 16:38:55 +0300

    Докеризация

---
+++ buildAndRun.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo 'Очистка файлов старой сборки...'
+rm build/libs/*.*
+rm docker/build/*.*
+echo 'Очистка контейнера...'
+sudo docker rm sonet
+sudo docker rmi docker-spring-boot-postgres
+echo 'Сборка...'
+./gradlew build
+cp build/libs/sonet-0.0.1-SNAPSHOT.jar docker/build/sonet-0.0.1-SNAPSHOT.jar
+echo 'Запуск контейнера...' 
+sudo docker compose up
